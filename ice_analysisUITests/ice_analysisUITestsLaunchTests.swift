5164f9ee22634e0c29f12da39a57b2d1ea9d327c
Tropical fish <wzc990503@gmail.com>
2022-07-28 11:43:22 +0800

    Initial Commit

---
+++ ice_analysisUITests/ice_analysisUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ice_analysisUITestsLaunchTests.swift
+//  ice_analysisUITests
+//
+//  Created by 王子诚 on 2022/7/28.
+//
+
+import XCTest
+
+class ice_analysisUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
